7b836c9b92991d3648ac1b1a041b852c8b33ba76
nikita.k@rcsmail.ru <n.kusyumov@mail.ru>
2019-09-04 18:10:47 +0300

    NavigationBar settings.

--- Example/ATNavigationBar/ExampleView.swift
+++ Example/ATNavigationBar/ExampleView.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+import UIKit
+
+class ExampleView: UIView {
+    
+    // MARK: - Internal properties
+    
+    private struct Appearance { }
+    private let appearance = Appearance()
+    
+    // MARK: - UI elements
+    
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        setupStyle()
+        addSubviews()
+        makeConstraints()
+    }
+    
+    private func setupStyle() {
+        
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func makeConstraints() {
+        
+    }
+    
+}
